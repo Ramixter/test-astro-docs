fca4e31a67adb61bf5ffc0e67a9551a346067429
ramixter1 <ramixalex@hotmail.com>
2024-06-23 03:11:14 +0200

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	base: '/test-astro-docs/',
+	site: 'https://ramixter.github.io/test-astro-docs/',
 	integrations: [
 		starlight({
 			title: 'My Docs',
